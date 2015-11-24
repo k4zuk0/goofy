9c90c60acdab9cc4b43281ee5d558e7ebd47e30b
k4zuk0 <anthony.moyart@wizbii.com>
2015-11-24 10:29:24 +0100

    style(format): replace tabs by spaces

--- server/src/fb.js
+++ server/src/fb.js
@@ -8,186 +8,186 @@ for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElemen
 mixpanel.init("2245181dbc803998dedc5b07d840e672");
 
 csssetup = function() {
-	head = document.head || document.getElementsByTagName( 'head' )[ 0 ];
-	style = document.createElement( 'style' );
-	style.type = 'text/css';
-	if ( style.styleSheet ) {
-			style.styleSheet.cssText = css;
-	} else {
-			style.appendChild( document.createTextNode( css ) );
-	}
-	head.appendChild( style );
+    head = document.head || document.getElementsByTagName( 'head' )[ 0 ];
+    style = document.createElement( 'style' );
+    style.type = 'text/css';
+    if ( style.styleSheet ) {
+            style.styleSheet.cssText = css;
+    } else {
+            style.appendChild( document.createTextNode( css ) );
+    }
+    head.appendChild( style );
 };
 
 function init() {
 
     csssetup();
 
-	setInterval(function() {
+    setInterval(function() {
 
-		window.dispatchEvent(new Event('resize'));
-		updateTitle();
-		dockCount();
+        window.dispatchEvent(new Event('resize'));
+        updateTitle();
+        dockCount();
 
-		var uploadButton = document.querySelector('._m._4q60._3rzn._6a');
-		if (uploadButton && uploadButton.onclick==null) {
-			uploadButton.onclick = function (e) {
-				e.preventDefault();
-				e.stopPropagation();
-				uploadInfo();
-			}
-		}
+        var uploadButton = document.querySelector('._m._4q60._3rzn._6a');
+        if (uploadButton && uploadButton.onclick==null) {
+            uploadButton.onclick = function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                uploadInfo();
+            }
+        }
 
-	}, 200);
+    }, 200);
 
-	setTimeout(function() {
-		//render settings menu
-		document.querySelector('._150g._30yy._2fug._p').click();
-		mixpanel.track("loaded");
-	}, 3000);
+    setTimeout(function() {
+        //render settings menu
+        document.querySelector('._150g._30yy._2fug._p').click();
+        mixpanel.track("loaded");
+    }, 3000);
 
-	document.body.onkeypress=function(e) {
-		// If no inputs are focused, or we're at the start of the message input (to prevent system beep), focus the message input and trigger the keypress.
-		if ((!document.querySelector(':focus') || (document.querySelector('._54-z:focus') && window.getSelection().baseOffset === 0)) && !e.metaKey && !e.ctrlKey) {
-			var char = event.which || event.keyCode;
+    document.body.onkeypress=function(e) {
+        // If no inputs are focused, or we're at the start of the message input (to prevent system beep), focus the message input and trigger the keypress.
+        if ((!document.querySelector(':focus') || (document.querySelector('._54-z:focus') && window.getSelection().baseOffset === 0)) && !e.metaKey && !e.ctrlKey) {
+            var char = event.which || event.keyCode;
 
-			// Focus the input at the end of any current text.
-			var el = document.querySelector('._54-z');
-			var range = document.createRange();
-			var sel = window.getSelection();
-			range.setStart(el, 1);
-			range.collapse(true);
-			sel.removeAllRanges();
-			sel.addRange(range);
+            // Focus the input at the end of any current text.
+            var el = document.querySelector('._54-z');
+            var range = document.createRange();
+            var sel = window.getSelection();
+            range.setStart(el, 1);
+            range.collapse(true);
+            sel.removeAllRanges();
+            sel.addRange(range);
 
-			// Trigger the captured key press.
-			var textEvent = document.createEvent('TextEvent');
-			textEvent.initTextEvent('textInput', true, true, null, String.fromCharCode(char), 9, "en-US");
-			el.dispatchEvent(textEvent);
+            // Trigger the captured key press.
+            var textEvent = document.createEvent('TextEvent');
+            textEvent.initTextEvent('textInput', true, true, null, String.fromCharCode(char), 9, "en-US");
+            el.dispatchEvent(textEvent);
 
-			return false;
-		}
-	};
+            return false;
+        }
+    };
 
-	document.querySelector('._5743').addEventListener("DOMSubtreeModified", function () {
-		updateTitle();
-	});
+    document.querySelector('._5743').addEventListener("DOMSubtreeModified", function () {
+        updateTitle();
+    });
 }
 
 function uploadInfo() {
-	window.webkit.messageHandlers.notification.postMessage({type: 'CHOOSE_IMAGE'});
+    window.webkit.messageHandlers.notification.postMessage({type: 'CHOOSE_IMAGE'});
 }
 
 function updateTitle() {
-	var a = ""
-	if (document.querySelector('._2v6o')) {
-		a = document.querySelector('._2v6o').textContent;
-	}
-	window.webkit.messageHandlers.notification.postMessage({type: 'SET_TITLE', title: document.querySelector('._5743 span').textContent, activity: a});
+    var a = ""
+    if (document.querySelector('._2v6o')) {
+        a = document.querySelector('._2v6o').textContent;
+    }
+    window.webkit.messageHandlers.notification.postMessage({type: 'SET_TITLE', title: document.querySelector('._5743 span').textContent, activity: a});
 }
 
 function newConversation() {
-	document.querySelector('._36ic._5l-3 > a._30yy').click();
+    document.querySelector('._36ic._5l-3 > a._30yy').click();
 }
 
 function gotoConversation(tag) {
-	if (tag==1) {
-		document.querySelector('._1ht2').nextElementSibling.firstChild.click();
-	} else {
-		document.querySelector('._1ht2').previousElementSibling.firstChild.click();
-	}
+    if (tag==1) {
+        document.querySelector('._1ht2').nextElementSibling.firstChild.click();
+    } else {
+        document.querySelector('._1ht2').previousElementSibling.firstChild.click();
+    }
 }
 
 function gotoConversationAtIndex(index) {
-	document.querySelector('._2xhi._5vn4 ul li:nth-child(' + index + ') a').click()
+    document.querySelector('._2xhi._5vn4 ul li:nth-child(' + index + ') a').click()
 }
 
 function reactivation(userid) {
-	if (userid) {
-		document.querySelector('[data-reactid="'+userid+'"] a').click();
-	} else if (new Date().getTime() < lastNotificationTime + 1000*60) {
-		document.querySelector('._1ht3 a').click();
-	}
+    if (userid) {
+        document.querySelector('[data-reactid="'+userid+'"] a').click();
+    } else if (new Date().getTime() < lastNotificationTime + 1000*60) {
+        document.querySelector('._1ht3 a').click();
+    }
 }
 
 function logout() {
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:last-child a').click();
+    document.querySelector('._54nq._2i-c._150g._558b._2n_z li:last-child a').click();
 }
 
 function info() {
-	document.querySelector('._fl3._30yy').click();
+    document.querySelector('._fl3._30yy').click();
 }
 
 function preferences() {
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:first-child a').click();
+    document.querySelector('._54nq._2i-c._150g._558b._2n_z li:first-child a').click();
 }
 
 function search() {
-	document.querySelector('._58al').focus()
+    document.querySelector('._58al').focus()
 }
 
 function dockCount() {
-	var c = document.querySelectorAll('._1ht3').length;
-	if (c != dockCounter) {
-		window.webkit.messageHandlers.notification.postMessage({type: 'DOCK_COUNT', content: String(c)});
-		dockCounter = c;
-	}
+    var c = document.querySelectorAll('._1ht3').length;
+    if (c != dockCounter) {
+        window.webkit.messageHandlers.notification.postMessage({type: 'DOCK_COUNT', content: String(c)});
+        dockCounter = c;
+    }
 
-	convertEmoji();
+    convertEmoji();
 
-	if (c > 0) {
-		var text = document.querySelector('._1ht3 ._1htf');
-		if (text) {
-			text = text.textContent;
-			var subtitle = document.querySelector('._1ht3 ._1ht6').textContent;
-			if (lastNotification != subtitle+text) {
-				var el = document.querySelector('._1ht3 ._1htf');
+    if (c > 0) {
+        var text = document.querySelector('._1ht3 ._1htf');
+        if (text) {
+            text = text.textContent;
+            var subtitle = document.querySelector('._1ht3 ._1ht6').textContent;
+            if (lastNotification != subtitle+text) {
+                var el = document.querySelector('._1ht3 ._1htf');
 
-				[].forEach.call(document.querySelectorAll('.emoticon_text'), function(a) {
-					a.textContent = "";
-				});
+                [].forEach.call(document.querySelectorAll('.emoticon_text'), function(a) {
+                    a.textContent = "";
+                });
 
-				text = document.querySelector('._1ht3 ._1htf').textContent;
+                text = document.querySelector('._1ht3 ._1htf').textContent;
 
-				var id = document.querySelector('._1ht1._1ht3').getAttribute('data-reactid');
-				var pictureUrl = document.querySelector('._1ht3 ._55lt > .img')
-				if (pictureUrl) {
-					pictureUrl = pictureUrl.getAttribute('src');
-				} else {
-					pictureUrl = "";
-				}
+                var id = document.querySelector('._1ht1._1ht3').getAttribute('data-reactid');
+                var pictureUrl = document.querySelector('._1ht3 ._55lt > .img')
+                if (pictureUrl) {
+                    pictureUrl = pictureUrl.getAttribute('src');
+                } else {
+                    pictureUrl = "";
+                }
 
-				//muted = ._569x
-				if (ignoreNotification || document.querySelector('[data-reactid="'+id+'"]').classList.toString().indexOf('_569x') > -1) {
-					ignoreNotification = false;
-				} else {
-					window.webkit.messageHandlers.notification.postMessage({type: 'NOTIFICATION', title: subtitle, text: text, id: id, pictureUrl: pictureUrl});
-					window.webkit.messageHandlers.notification.postMessage({type: 'DOCK_COUNT', content: String(c)});
-				}
+                //muted = ._569x
+                if (ignoreNotification || document.querySelector('[data-reactid="'+id+'"]').classList.toString().indexOf('_569x') > -1) {
+                    ignoreNotification = false;
+                } else {
+                    window.webkit.messageHandlers.notification.postMessage({type: 'NOTIFICATION', title: subtitle, text: text, id: id, pictureUrl: pictureUrl});
+                    window.webkit.messageHandlers.notification.postMessage({type: 'DOCK_COUNT', content: String(c)});
+                }
 
-				lastNotification = subtitle+text;
-				lastNotificationTime = new Date().getTime();
-			}
-		}
-	}
+                lastNotification = subtitle+text;
+                lastNotificationTime = new Date().getTime();
+            }
+        }
+    }
 }
 
 function replyToNotification(userid, answer) {
-	document.querySelector('[data-reactid="'+userid+'"] a').click();
-	setTimeout(function () {
-		var textEvent = document.createEvent('TextEvent');
-		textEvent.initTextEvent('textInput', true, true, null, answer, 9, "en-US");
-		document.querySelector('._209g._2vxa').dispatchEvent(textEvent);
-		ignoreNotification = true;
-		__triggerKeyboardEvent(document.querySelector('._209g._2vxa'),13,true);
-	},50);
+    document.querySelector('[data-reactid="'+userid+'"] a').click();
+    setTimeout(function () {
+        var textEvent = document.createEvent('TextEvent');
+        textEvent.initTextEvent('textInput', true, true, null, answer, 9, "en-US");
+        document.querySelector('._209g._2vxa').dispatchEvent(textEvent);
+        ignoreNotification = true;
+        __triggerKeyboardEvent(document.querySelector('._209g._2vxa'),13,true);
+    },50);
 }
 
 // Handle pasted image data forwarded from the wrapper app.
 function pasteImage(base64Data) {
-	var blob = b64toBlob(base64Data, 'image/png');
-	var uploader = getPhotoUploadComponent();
-	uploader._instance.uploadFile(blob);
+    var blob = b64toBlob(base64Data, 'image/png');
+    var uploader = getPhotoUploadComponent();
+    uploader._instance.uploadFile(blob);
 }
 
 // Convert base64 encoded data to a Blob.
@@ -217,31 +217,31 @@ function b64toBlob(b64Data, contentType, sliceSize) {
 
 // Find the instance of the photo upload class from the React component tree.
 function getPhotoUploadComponent() {
-	var id = document.querySelector('._4rv4 form').getAttribute('data-reactid');
-	var idComponents = id.split('.');
-	var children = __REACT_DEVTOOLS_GLOBAL_HOOK__._reactRuntime.Mount._instancesByReactRootID;
-	var component;
+    var id = document.querySelector('._4rv4 form').getAttribute('data-reactid');
+    var idComponents = id.split('.');
+    var children = __REACT_DEVTOOLS_GLOBAL_HOOK__._reactRuntime.Mount._instancesByReactRootID;
+    var component;
 
-	for (var i = 1; i < idComponents.length; i++) {
-		var rootId = '.' + idComponents[i];
-		component = children[rootId];
-		children = getChildren(component);
-	}
+    for (var i = 1; i < idComponents.length; i++) {
+        var rootId = '.' + idComponents[i];
+        component = children[rootId];
+        children = getChildren(component);
+    }
 
-	return component;
+    return component;
 }
 
 // Unwrap the children from a React class instance.
 function getChildren(instance) {
-	var children = null;
-	if (instance._renderedComponent) {
-		children = getChildren(instance._renderedComponent);
-	} else if (instance._renderedChildren) {
-		children = instance._renderedChildren;
-	} else {
-		children = [];
-	}
-	return children;
+    var children = null;
+    if (instance._renderedComponent) {
+        children = getChildren(instance._renderedComponent);
+    } else if (instance._renderedChildren) {
+        children = instance._renderedChildren;
+    } else {
+        children = [];
+    }
+    return children;
 }
 
 function __triggerKeyboardEvent(el, keyCode, meta) {
@@ -254,23 +254,23 @@ function __triggerKeyboardEvent(el, keyCode, meta) {
 
     eventObj.keyCode = keyCode;
     eventObj.which = keyCode;
-	if (meta) {
-		eventObj.metaKey = true;
-	}
+    if (meta) {
+        eventObj.metaKey = true;
+    }
 
     el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj);
 
 }
 
 function convertEmoji() {
-	var emoticon = document.querySelectorAll('.emoticon, ._1az');
-	[].forEach.call(emoticon, function(e) {
-		e.classList.toString().split(" ").forEach(function (c) {
-			if (EMOJI_TABLE[c]) {
-				e.parentNode.replaceChild(document.createTextNode(EMOJI_TABLE[c]),e);
-			}
-		});
-	});
+    var emoticon = document.querySelectorAll('.emoticon, ._1az');
+    [].forEach.call(emoticon, function(e) {
+        e.classList.toString().split(" ").forEach(function (c) {
+            if (EMOJI_TABLE[c]) {
+                e.parentNode.replaceChild(document.createTextNode(EMOJI_TABLE[c]),e);
+            }
+        });
+    });
 }
 
 
